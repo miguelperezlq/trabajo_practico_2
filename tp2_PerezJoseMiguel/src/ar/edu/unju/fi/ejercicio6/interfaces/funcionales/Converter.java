c261f2ae4b4b643c37b75023b9b7dada212657a7
miguel <miguel@localhost>
2024-05-08 02:25:13 -0300

    Ejercicio 06 terminado

--- tp2_PerezJoseMiguel/src/ar/edu/unju/fi/ejercicio6/interfaces/funcionales/Converter.java
+++ tp2_PerezJoseMiguel/src/ar/edu/unju/fi/ejercicio6/interfaces/funcionales/Converter.java
@@ -1,5 +1,15 @@
 package ar.edu.unju.fi.ejercicio6.interfaces.funcionales;
 
-public interface Converter {
+@FunctionalInterface
+public interface Converter<T,T1> {
+	T1 convert(T t);
+	
+	static <T> boolean isNotNull(T t) {
+		return t != null;
+	}
+	
+	default void mostrarObjeto(T1 t1) {
+		System.out.println("Objeto - " + t1.toString());
+	}
 
 }
--- tp2_PerezJoseMiguel/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ tp2_PerezJoseMiguel/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,10 +1,38 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
+import ar.edu.unju.fi.ejercicio6.interfaces.funcionales.Converter;
+import ar.edu.unju.fi.ejercicio6.model.FelinoDomestico;
+import ar.edu.unju.fi.ejercicio6.model.FelinoSalvaje;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//EJEMPLO
+		FelinoDomestico gato = new FelinoDomestico("Garfield", (byte)45, 12f);
+		//definición de expresión lambda que define el convertidor de FelinoDomestico a //FelinoSalvaje.
+		Converter<FelinoDomestico, FelinoSalvaje> converter = x -> new FelinoSalvaje(x.getNombre(), x.getEdad(), x.getPeso());
+		//se realiza la conversión
+		FelinoSalvaje felino1 = converter.convert(gato);
+		//mostramos los datos del objeto felino salvaje felino1
+		converter.mostrarObjeto(felino1);
+		
+        //EJERCICIO06
+        FelinoSalvaje felinoSalvaje = new FelinoSalvaje("Tanner", (byte) 20, 186f);
+        // Verificar que el objeto no sea nulo utilizando el método estático isNotNull()
+        if (Converter.isNotNull(felinoSalvaje)) {
+            //convertiendo FelinoSalvaje a FelinoDomestico
+            Converter<FelinoSalvaje, FelinoDomestico> converter1 = x ->
+                new FelinoDomestico(x.getNombre(), x.getEdad(), x.getPeso());
+            
+            // Realizar la conversión
+            FelinoDomestico felinoDomestico = converter1.convert(felinoSalvaje);
+            
+            // Mostrar los datos del objeto FelinoDomestico utilizando el método mostrarObjeto()
+            converter1.mostrarObjeto(felinoDomestico);
+        } else {
+            System.out.println("El objeto felinoSalvaje es nulo.");
+        }
+        
+        
 
-	}
-
+    }
 }
